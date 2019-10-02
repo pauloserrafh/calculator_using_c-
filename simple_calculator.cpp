fabd3e4f0e4fe33f6d0fb5a3dff5b9bd59a8fc4e
Gaurav Bharti <40711063+realme72@users.noreply.github.com>
2019-10-02 18:46:12 +0530

    Update simple_calculator.cpp
    added just user response thing

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -16,5 +16,47 @@ int main(){
     //if you want to find the logarithmic of a no.s a and b
     cout<<"\n log of a & b :";
     cout<<log(a)<<" "<<log(b);
-  
+     int sum;
+     string userResponse;
+     char userinput;
+     cout << "\t / = Divide, * = Multiply";
+     cout << "\n\nEnter a calculation: ";
+     cin >> a;
+     cin >> userinput;
+     cin >> b;
+
+
+    switch(userinput){
+        case '+' : sum = a + b;
+        break;
+        case '-' : sum = a - b;
+        break;
+        case '*' : sum = a * b;
+        break;
+        case '/' : sum = a / b;
+
+
+    }
+
+    cout << "\nThe total of those numbers are: " << sum << endl;
+    if(sum > 20){
+      cout << "\nHa, your number is greater than 20 \n";
+      }
+       if(sum < 20){
+       cout << "\nCan't catch me out, your number is less than 20 \n";
+       }
+       if(sum == 20){
+       cout << "\nYour number is equal to 20 \n";
+       }
+
+       cout << "\nDoes this calculator work correctly? (Yes/No) \n";
+       cin >> userResponse;
+
+       if(userResponse == "Yes"){
+       cout << "Thanks for your honesty \n";
+       }
+       if(userResponse == "No"){
+       cout << "I know it works so stop lying";
+       }
+
 }
